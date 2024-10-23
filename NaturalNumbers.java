483b47f0b91137fc4de841c712ad8fabf7c762eb
ayeshamubeen26 <ayeshamubeen15@gmail.com>
2024-10-24 00:47:36 +0500

    Update and rename Main.java to NaturalNumbers.java

--- NaturalNumbers.java
+++ NaturalNumbers.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class NaturalNumbers {
     public static void main(String[] args) {
         int sum, n, i;
         Scanner sc = new Scanner(System.in);
